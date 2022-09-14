23c84b31c0e581dea32761a3408fdc1249255b38
jbundziow <jakub.bundziow@gmail.com>
2022-09-14 22:15:37 +0200

    Auto mode works partly, finish it later

--- script.js
+++ script.js
@@ -6,18 +6,42 @@
 
 
   //TEST!!!! - clock
-
-  function addLeadingZeros(num, totalLength) { //it converts eg. '3' mins into '03' mins
+  /* function addLeadingZeros(num, totalLength) { //it converts eg. '3' mins into '03' mins
     return String(num).padStart(totalLength, '0');
   }
   const currentDate = new Date();
   const actualTime = addLeadingZeros(currentDate.getHours(), 2) + ":" + addLeadingZeros(currentDate.getMinutes(), 2);
-  document.getElementById("auto-hour-start").textContent = actualTime; //new text
+  document.getElementById("auto-hour-start").textContent = actualTime; //new text */
+
+
+//MAIN CODE
+
+document.getElementById("stop-button").disabled = true; //disable button 'Stop' on the beginning
 
 /* FUNCTION THAT CONVERTS MINUTES INTO '#h #min' */
 convertMinutesToHours = (mins) => parseInt(mins/60) +"h " + parseInt(mins%60) + " min";
 
-/* COUNT TOTAL ELAPSED TIME AND DISPLAY IT ON SCREEN */
+/* FUNCTION THAT CONVERTS Date() INTO HH:MM */
+function convertDateObjectIntoHHMM(MilisecondsSince1970) {
+  var d = new Date(MilisecondsSince1970);
+  return addLeadingZeros(d.getHours(),2) + ":" + addLeadingZeros(d.getMinutes(),2);
+}
+
+/* FUNCTION THAT CONVERTS MILISECONDS INTO MINUTES */
+convertMilisecondsToMinutes = (miliseconds) => parseInt(miliseconds/1000/60);
+
+  /* RETURN ID OF THE NEXT RECORD IN TABLE */
+  function nextTableID () {
+    let myTable = document.getElementById("history-table");
+    return myTable.rows.length; /* this is ID */
+  }
+
+  /* FUNCTION THAT CONVERTS E.G. '3' MINS INTO '03' MINS */
+  function addLeadingZeros(num, totalLength) {
+    return String(num).padStart(totalLength, '0');
+  }
+
+/* COUNT TOTAL ELAPSED TIME FROM TABLE AND DISPLAY IT ON SCREEN */
 setInterval(function () {
     var myTable = document.getElementById("history-table");
   
@@ -29,6 +53,20 @@ setInterval(function () {
       document.getElementById("total-elapsed-time").textContent = convertMinutesToHours(sum); //sum coverted into hours and displayed on screen
     
   }, 100); //repeat every 100ms
+
+  /* SHOW CURRENT ELAPSED TIME SINCE START BUTTON IS CLICKED IN AUTO MODE */
+setInterval(function () {
+  if (document.getElementById("start-button").disabled == true)
+  {
+  const currentDate = new Date();
+  let actualLearningTimeInMinutes = convertMilisecondsToMinutes(currentDate.getTime() - timeWhenStartButtonWasClicked);
+  document.getElementById("auto-elapsed-time").textContent = convertMinutesToHours(actualLearningTimeInMinutes);
+  }
+  else
+  {
+    document.getElementById("auto-elapsed-time").textContent = " - ";
+  }
+}, 100); //repeat every 100ms
 
   /* SELECT MODE -> SWITCHING WINDOWS */
   setInterval(function () {
@@ -62,20 +100,17 @@ setInterval(function () {
 
   //modify table
 
-
   function addRecord_ManualMode() {
-
-      /* count rows in table */
-      var myTable = document.getElementById("history-table");
-      var totalRowCount = myTable.rows.length; /* this is ID */
-
+    //get elements
     let minutesFromInput = document.getElementById(
       "manual-entered-minutes"
     ).value;
     let commentFromInput = document.getElementById("textarea-comment").value;
     let table = document.querySelector("#history-table tbody");
+
+    //enter record to the table
     table.innerHTML +=
-      "<tr><td>" + totalRowCount + 
+      "<tr><td>" + nextTableID() + 
       "</td><td>-</td><td>-</td><td>" +
       minutesFromInput +
       "</td><td>" +
@@ -87,10 +122,49 @@ setInterval(function () {
       document.getElementById("textarea-comment").value = "";
   }
 
+  var timeWhenStartButtonWasClicked;
+  function startButtonPushed () {
+    //disable buttons and enable stop-button
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("dropdown-auto-manual").disabled = true;
+    document.getElementById("stop-button").disabled = false;
 
+    //save time of click start button in global variable
+    const currentDate = new Date();
+    timeWhenStartButtonWasClicked = currentDate.getTime();
+    
+    //show hour when start button was clicked in 'started at' field
+    const actualTime = addLeadingZeros(currentDate.getHours(), 2) + ":" + addLeadingZeros(currentDate.getMinutes(), 2);
+    document.getElementById("auto-hour-start").textContent = actualTime;
 
+    //current elapsed time counts by one of setInterval function above
+  }
 
-function addRecord_AutoMode(from, to, comment) {
+  function stopButtonPushed () {
+    //save time of click stop button in variable
+    const currentDate = new Date();
+    let timeWhenStopButtonWasClicked = currentDate.getTime();
+    
+
+    //from
+    console.log(convertDateObjectIntoHHMM(timeWhenStartButtonWasClicked)); //TODO: works OK, put it later into table
+
+    //to
+    console.log(convertDateObjectIntoHHMM(timeWhenStopButtonWasClicked)); //TODO: works OK, put it later into table
+
+    //mins
+    console.log(convertMilisecondsToMinutes(timeWhenStopButtonWasClicked-timeWhenStartButtonWasClicked)); //TODO: works OK, put it later into table
+
+    //TODO:
+    //add record into the table
+    //'select mode', 'start' enabled; 'stop' disabled
+    //started at - clear it from 22:05 into " - "
+    //at the end:
+    //timeWhenStartButtonWasClicked = 0;
+    //timeWhenStopButtonWasClicked = 0;
+  }
+
+function addRecord_AutoMode(id, from, to, mins, comment) {
   //TODO
   let table = document.querySelector("#history-table tbody");
   table.innerHTML +=
